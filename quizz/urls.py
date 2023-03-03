fc7ff2d8b0269e424c1d5eb2719eed29e7e6b9f1
holequoctin <holequocti@gmail.com>
2023-03-03 08:57:43 +0700

    complete authens form

--- quizz/urls.py
+++ quizz/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,include
-from .views import *
+from .views import *\
+
+app_name = 'quizz'
 urlpatterns = [
-    path('',landing_page,name = 'LandingPage'),
+    path('',landing_view,name = 'LandingPage'),
+    path('login/',login_view,name='Login'),
+    path('register/',register_view,name='Register')
 ]
--- quizz/views.py
+++ quizz/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def landing_page(request):
+def landing_view(request):
+    context = {
+        'title' : 'Trang chủ'
+    }
     return render(request,'landing_page.html',{})
+
+def login_view(request):
+    context = {
+        'title' : 'Đăng nhập'
+    }
+    return render(request,'login.html',context)
+
+def register_view(request):
+    context = {
+        'title' : 'Register'
+    }
+    return render(request,'register.html',context)
